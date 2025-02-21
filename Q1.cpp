9882a801256b7a95ec63a0f4a2a8710042dc7d67
faruky <faruk.yilmaz@stonybrook.edu>
2025-02-21 15:16:29 -0500

    Final submission

--- Q1.cpp
+++ Q1.cpp
@@ -8,10 +8,10 @@ struct tuple{
     T1 t1;
     T2 t2;
     T3 t3;
-    tuple(T1 t1, T2 t2, T3 t3) : t1(t1), t2(t2), t3(t3) {} // might not be best practice with naming  
+    tuple(T1 t1, T2 t2, T3 t3) : t1(t1), t2(t2), t3(t3) {} 
     tuple(){}
 };
-struct df{
+struct df{ //struct to evaluate the derivative of the given function at a point x 
     long double operator() (long double x){
         return (-3 * pow(x, 2)) * (exp(-pow(x, 3))) - (4 * pow(x, 3)) - cos(x);
     }
@@ -19,9 +19,14 @@ struct df{
 struct f{
     df df;
     int iterations = 0; int floating_point_operations = 0;
-   long double operator() (long double x){
+   long double operator() (long double x){ //evaluates the function at a given point
         return exp(-pow(x, 3)) - pow(x, 4) - sin(x);
    }
+   /*
+   * takes interval to perform bisection on
+   * returns tuple containing the root found within given tolerance, 
+   * floating point operations and iterations done by method
+   */
    tuple<long double, int, int> bisection(long double a, long double b){
     iterations = floating_point_operations = 0;
     tuple<long double, int, int> bisection_return; 
@@ -35,10 +40,12 @@ struct f{
         floating_point_operations += 3;
         iterations++;
     }
-    bisection_return.t1 = ((a + b) / 2.0); bisection_return.t2 = floating_point_operations; bisection_return.t3 = iterations;
+    bisection_return.t1 = ((a + b) / 2.0), bisection_return.t2 = floating_point_operations, bisection_return.t3 = iterations;
     return bisection_return;
    }
-
+    /*
+    *Newton's method given an initial guess, finds root within threshold
+    */
     tuple<long double, int, int> newton(long double init_guess){
         iterations = floating_point_operations = 0;
         tuple<long double, int, int> newton_return;
@@ -52,11 +59,14 @@ struct f{
         newton_return.t1 = init_guess; newton_return.t2 = floating_point_operations; newton_return.t3 = iterations; 
         return newton_return;
     }
+    /*
+    *Finds root within threshold given initial a_1, a_2
+    */
     tuple<long double, int, int> secant(long double a, long double b){
         floating_point_operations = iterations = 0;
         tuple<long double, int, int> secant_return;
         long double new_b = 0;
-        while(!(abs((*this)(a)) < 5e-4)){
+        while(!(abs((*this)(a)) < 5e-6)){
             new_b = a;
             a = a - (*this)(a) / (((*this)(a) - (*this)(b)) / (a - b));
             b = new_b; 
@@ -66,14 +76,17 @@ struct f{
         secant_return.t1 = a; secant_return.t2 = floating_point_operations; secant_return.t3 = iterations;
         return secant_return;
     }
-
+    /*
+    *Randomly chooses values within given interval until one is within acceptable threshold of root 
+    *Uses <random> instead of rand() function because they are more mathematically random 
+    */
     tuple<long double, int, int> monte_carlo(long double lower, long double upper){
         floating_point_operations = iterations = 0;
         std::random_device rd;
         std::uniform_real_distribution<long double> dist(lower, upper);
         tuple<long double, int, int> mc_return;
         long double guess = dist(rd);
-        while(!(abs((*this)(guess)) < 1e-6)){
+        while(!(abs((*this)(guess)) < 5e-6)){
             guess = dist(rd);
             iterations++;
             floating_point_operations++;
@@ -81,59 +94,70 @@ struct f{
             mc_return.t1 = guess; mc_return.t2 = floating_point_operations; mc_return.t3 = iterations;
             return mc_return;
     }
-
+    /*
+    * Runs previous Monte Carlo algorithm with however many threads the program has access to
+    * Stops when a thread finds a solution within the acceptable threshold
+    */
     tuple<long double, int, int> monte_carlo_parallel(long double lower, long double upper){
         bool found_solution = false;
         floating_point_operations = iterations = 0;
         std::random_device rd;
         std::uniform_real_distribution<long double> dist(lower, upper);
         tuple<long double, int, int> result;
-        #pragma omp parallel shared(found_solution)
+        #pragma omp parallel shared(found_solution) //found solution is shared between all threads
         {
-        int iterations_thread = 0;  // Local variable for each thread
-        int floating_point_operations_thread = 0;  // Local variable for each thread
+        // local variables for each thread
+        int iterations_thread = 0;  
+        int floating_point_operations_thread = 0; 
 
-        long double guess = dist(rd);
-        while (!found_solution) {
+        long double guess = dist(rd); //random guess
+        while (!found_solution) { //if another thread has not found a solution execute code 
             guess = dist(rd);
             floating_point_operations_thread++;
             iterations_thread++;
             
-            if (abs((*this)(guess)) < 5e-4) {
-                #pragma omp atomic write
+            if (abs((*this)(guess)) < 5e-6) {
+                #pragma omp atomic write //ensures only one thread can write to the memory of found_solution
                 found_solution = true;
                 result = tuple<long double, int, int>(guess, floating_point_operations_thread, iterations_thread);
             }
         }
 
-        #pragma omp atomic
+        #pragma omp atomic 
         floating_point_operations += floating_point_operations_thread;
         
         #pragma omp atomic
         iterations += iterations_thread;
     }
 
-    // Now return the result with accumulated values
-    result.t2 = floating_point_operations;  // Total floating point operations
-    result.t3 = iterations;  // Total iterations
+    //return result with accumulated values
+    result.t2 = floating_point_operations;  // total floating point operations
+    result.t3 = iterations;  // total iterations
     return result;
     }
 };
 int main(){
     f f;
+    /*
+    * Declares tuples containing all information for specificied initial conditions for each algo
+    */
     tuple<long double, int, int> newton_return = f.newton(0);
     tuple<long double, int, int> secant_return = f.secant(-1, 1);
     tuple<long double, int, int> bisection_return = f.bisection(-1, 1);
+
     auto start_mcp = std::chrono::high_resolution_clock::now();
-    tuple<long double, int, int> mc_return_parallel = f.monte_carlo_parallel(0.5, 0.75);
+    tuple<long double, int, int> mc_return_parallel = f.monte_carlo_parallel(-10, 10);
     auto end_mcp = std::chrono::high_resolution_clock::now();
+
     auto start_mc = std::chrono::high_resolution_clock::now();
-    tuple<long double, int, int> mc_return = f.monte_carlo(0.5, 0.75);
+    tuple<long double, int, int> mc_return = f.monte_carlo(-10, 10);
     auto end_mc = std::chrono::high_resolution_clock::now();
+
     std::chrono::duration<double> duration_mc = (end_mc - start_mc);
     std::chrono::duration<double> duration_mcp = (end_mcp - start_mcp);
     long double time_per_op_mc = mc_return.t2 / duration_mc.count();
     long double time_per_op_mcp = mc_return.t2 / duration_mcp.count();
+
     std::cout << "Bisection method with initial bracketing [-1, 1]: \n" << " Root:"<< std::fixed << std::setprecision(16) << bisection_return.t1 << std::endl
     << "    floating point operations: " << bisection_return.t2 << std::endl
     << "    iterations: " << bisection_return.t3 << std::endl
--- Q2.cpp
+++ Q2.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<Eigen/Dense>
-
+//evaluates the function given the coefficient vector and the input to evaluate at
 double func(const Eigen::VectorXd& coeff, int value){
     double eval;
     for(int i = 0; i < coeff.size(); i++){
@@ -9,18 +9,58 @@ double func(const Eigen::VectorXd& coeff, int value){
     return eval;
 }
 int main(){
+    /*
+    * Question 2.1
+    * Initialize 5 x 5 matrix where each row has 
+    */
     int rows = 5; int cols = 5;
-    Eigen::MatrixXd x_values(rows, cols);
-    Eigen::VectorXd y_values(5);
+    /* Matrix that hold target vectors x values evaluated to the (column_size - 1) power
+    * Given by page 6 of topic 2 notes
+    */
+    Eigen::MatrixXd x_values(rows, cols); 
+    Eigen::VectorXd target(5);
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < cols; j++){
-            x_values(i, j) = pow((rows - i), cols - 1 - j);
+            x_values(i, j) = pow((rows - i), cols - 1 - j); 
         }
     }
-    y_values << 417, 398, 397, 407, 412; 
-    Eigen::VectorXd solution_vec(5);
-    solution_vec = x_values.colPivHouseholderQr().solve(y_values);;
+
+    target << 417, 398, 397, 407, 412; //target vector with values to be interpolated
+    Eigen::VectorXd solution_vec(5); //gives coefficients
+    solution_vec = x_values.colPivHouseholderQr().solve(target); //Uses Eigen's QR decompisition and then solves to give solution vec
+    std::cout<< "The interpolated polynomial is given by: ";
     for(int i = 0; i < solution_vec.size(); i++){
-        std::cout << solution_vec(i) << std::endl;
+        if(i != solution_vec.size() - 1) 
+            std::cout << solution_vec(i) << "x^" << (solution_vec.size() - 1 - i) << " + "; //outputs the polynomial to terminal
+        else
+            std::cout << solution_vec(i) << std::endl;
     }
+    std::cout << "The interpolated polynomial evaluated at P(6) = " << func(solution_vec, 6) << std::endl;
+
+    /*
+    * Question 2.2
+    * Quadratic Least Squares from given
+    * 417       1 5 25      
+    * 398       1 4 16      b
+    * 397   =   1 3 9   *   m_1
+    * 407       1 2 4       m_2
+    * 412       1 1 1
+    */
+   Eigen::MatrixXd X(5, 3);
+   for(int i = 0; i < X.rows(); i++){
+    for(int j = 0; j < X.cols(); j++){
+        X(i, j) = pow(cols - i, j); //populates matrix accordingly
+    }
+   }
+    //Solves Beta^ = (X^T * X)(X^T) * y^ 
+   Eigen::Vector3d quad_coeff = (X.transpose() * X).inverse() * X.transpose() * target; 
+   std::cout << "The equation for a quadratic line of best fit: ";
+   for(int i = quad_coeff.size() - 1; i >= 0; i--){
+    if(i != 0)
+        std::cout << quad_coeff(i) << "x^" << i << " + ";
+    else
+        std::cout << quad_coeff(i) << std::endl;
+   }
+   std::cout << "The quadratic fit evaluated at t = 6 is: " << func(quad_coeff.reverse(), 6);
+
 }
